65d008d7b9cc3fdb3e0db6f45e6f575c37e004bc
mubeenaftab <mubeenaftab66@gmail.com>
2024-10-12 19:05:05 +0500

    updated route

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://aibot-healthcare.onrender.com/',
 });
 
 api.interceptors.request.use(
